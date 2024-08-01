ae563c675900d42f3920f1a76ea4b14393201d86
terrylin <terrylin@aslcn.com.cn>
2024-08-01 10:02:24 +0800

    'update'

--- temp.sql
+++ temp.sql
@@ -1,50 +1,148 @@
+create or replace PROCEDURE "RPT_STH_R031_PROC" (
+    i_start_date IN VARCHAR2,
+    i_end_date IN VARCHAR2,
+	o_ret_val IN OUT SYS_REFCURSOR
+) AS
+BEGIN
+OPEN o_ret_val FOR
+WITH year_month_12 AS (
+	SELECT ADD_MONTHS(TO_DATE(i_start_date,'YYYY-MM'),Rownum -1) as year_month
+    FROM dual
+	CONNECT BY Rownum <= 12
+),
+year_month_search AS (
+	SELECT ADD_MONTHS(TO_DATE(i_start_date,'YYYY-MM'),Rownum -1) as year_month
+    FROM dual
+	CONNECT BY Rownum <= (SELECT MONTHS_BETWEEN(to_date(i_end_date,'YYYY-MM'), to_date(i_start_date,'YYYY-MM')) FROM dual)+1
+),
+activation_count_list as (
+    select TRUNC(acct_efft_dtm, 'MM') as activation_month,count(ONLINE_BOOKING_USER_ID) as activation_count
+    from UAM_ONLINE_BOOKING_USER
+    where acct_efft_dtm is not null
+    AND acct_efft_dtm >= TO_DATE(i_start_date,'YYYY-MM')
+    AND acct_efft_dtm < add_months(TO_DATE(i_end_date,'YYYY-MM'), 1)
+    group by TRUNC(acct_efft_dtm, 'MM')
+),
+encntr_by_date as (
+    select TRUNC(sdt, 'MM') as sdt_month,ENCNTR_ID
+    from CLC_ENCNTR
+    where (IS_CANCEL is null or IS_CANCEL=0)
+    AND ENCNTR_STS <> 'D'
+    AND sdt >= TO_DATE(i_start_date,'YYYY-MM')
+    AND sdt < add_months(TO_DATE(i_end_date,'YYYY-MM'), 1)
+    AND site_id in(
+        select site_id
+        from cmn_site
+        where svc_cd='STH'
+        and is_mock_up=0
+    )
+),
+attend_count_list as (
+    select sdt_month,count(ENCNTR_ID) as attend_count
+    from(
+        select ce.* from encntr_by_date ce
+        INNER JOIN STH_S_WAITING_QUEUE q ON q.ENCNTR_ID = ce.ENCNTR_ID and VISIT_POINT_CD='5' and q.is_del=0
+    )group by sdt_month
+),
+percentage_list as (
+    select year_month,
+        case when nvl(atte.attend_count,0)=0 then 'N/A'
+        when nvl(acti.activation_count,0)=0 then '0'
+        else trim(to_char(acti.activation_count/atte.attend_count, '9999990.9')||'%') end as percentage
+    from year_month_search ym
+    left join activation_count_list acti on ym.year_month=acti.activation_month
+    left join attend_count_list atte on ym.year_month=atte.sdt_month
+),
+all_list as (
+    select yms.year_month,
+        'No. of Attendance' as count_desc,
+        1 AS order_seq,
+        to_char(nvl(atte.attend_count,0)) as count_num
+    from year_month_search yms
+    left join attend_count_list atte on yms.year_month=atte.sdt_month
+    union all
+    select yms.year_month,
+        'No. of Activation' as count_desc,
+        2 AS order_seq,
+        to_char(nvl(acti.activation_count,0)) as count_num
+    from year_month_search yms
+    left join activation_count_list acti on yms.year_month=acti.activation_month
+    union all
+    select year_month,
+        '(Total no. of Activation / No. of Attendance) x 100%' as count_desc,
+        3 AS order_seq,
+        percentage as count_num
+    from percentage_list
+)
+select count_desc,TO_CHAR(year_month,'Mon RR','NLS_DATE_LANGUAGE=AMERICAN') as year_month_str,count_num
+from all_list
+order by order_seq,year_month
+;
+END;
 
-WITH MONTH_YEAR AS (
-	SELECT TO_CHAR(ADD_MONTHS(TO_DATE(:i_start_date,'YYYY-MM'),Rownum -1),'MM/YY') AS MONTH_YEAR FROM DUAL
-	CONNECT BY Rownum <= (SELECT MONTHS_BETWEEN(to_date(:i_end_date,'YYYY-MM'), to_date(:i_start_date,'YYYY-MM'))+1 FROM dual)
+
+
+
+create or replace PROCEDURE "RPT_STH_R031_CHART_PROC" (
+    i_start_date IN VARCHAR2,
+    i_end_date IN VARCHAR2,
+	o_ret_val IN OUT SYS_REFCURSOR
+) AS
+BEGIN
+OPEN o_ret_val FOR
+WITH year_month_12 AS (
+	SELECT ADD_MONTHS(TO_DATE(i_start_date,'YYYY-MM'),Rownum -1) as year_month
+    FROM dual
+	CONNECT BY Rownum <= 12
+),
+year_month_search AS (
+	SELECT ADD_MONTHS(TO_DATE(i_start_date,'YYYY-MM'),Rownum -1) as year_month
+    FROM dual
+	CONNECT BY Rownum <= (SELECT MONTHS_BETWEEN(to_date(i_end_date,'YYYY-MM'), to_date(i_start_date,'YYYY-MM')) FROM dual)+1
+),
+activation_count_list as (
+    select TRUNC(acct_efft_dtm, 'MM') as activation_month,count(ONLINE_BOOKING_USER_ID) as activation_count
+    from UAM_ONLINE_BOOKING_USER
+    where acct_efft_dtm is not null
+    AND acct_efft_dtm >= TO_DATE(i_start_date,'YYYY-MM')
+    AND acct_efft_dtm < add_months(TO_DATE(i_end_date,'YYYY-MM'), 1)
+    group by TRUNC(acct_efft_dtm, 'MM')
+),
+encntr_by_date as (
+    select TRUNC(sdt, 'MM') as sdt_month,ENCNTR_ID
+    from CLC_ENCNTR
+    where (IS_CANCEL is null or IS_CANCEL=0)
+    AND ENCNTR_STS <> 'D'
+    AND sdt >= TO_DATE(i_start_date,'YYYY-MM')
+    AND sdt < add_months(TO_DATE(i_end_date,'YYYY-MM'), 1)
+    AND site_id in(
+        select site_id
+        from cmn_site
+        where svc_cd='STH'
+        and is_mock_up=0
+    )
+),
+attend_count_list as (
+    select sdt_month,count(ENCNTR_ID) as attend_count
+    from(
+        select ce.* from encntr_by_date ce
+        INNER JOIN STH_S_WAITING_QUEUE q ON q.ENCNTR_ID = ce.ENCNTR_ID and VISIT_POINT_CD='5' and q.is_del=0
+    )group by sdt_month
+),
+count_list as (
+    select year_month,
+        nvl(atte.attend_count,0) as attend_count,
+        nvl(acti.activation_count,0) as activation_count
+    from year_month_search ym
+    left join activation_count_list acti on ym.year_month=acti.activation_month
+    left join attend_count_list atte on ym.year_month=atte.sdt_month
 )
-,STH_CLINIC AS (
-	SELECT SITE_ID,SITE_CD FROM CMN_SITE cs WHERE cs.SVC_CD ='STH' AND nvl(cs.IS_MOCK_UP,0) = 0 AND cs.SITE_ID = DECODE(:i_siteId,'-',cs.SITE_ID,:i_siteId)
+select TO_CHAR(year_month,'Mon RR','NLS_DATE_LANGUAGE=AMERICAN') as year_month_str,attend_count,activation_count,rownum*10 as horizontal_axis  from (
+    select ym.year_month,attend_count,activation_count
+    from year_month_12 ym
+    left join count_list cl on cl.year_month=ym.year_month
+    order by ym.year_month
 )
-,SHSC_AA_ENCNTRTYPE_IDS AS (
-	SELECT ENCNTR_TYPE_ID FROM (
-		SELECT cet.ENCNTR_TYPE_ID, cet.SITE_ID FROM CLN_ENCNTR_TYPE cet 	
-		WHERE cet.SVC_CD ='STH' AND cet.ENCNTR_TYPE_CD ='SHSC-AA'
-	) cet
-	WHERE cet.SITE_ID IS NULL OR (cet.SITE_ID IN (SELECT SITE_ID FROM STH_CLINIC))
-)
-,DATE_RANGE_APPT_IDS AS (
-	SELECT ala.CREATE_DTM , ala.APPT_ID 
-	FROM ANA_LOG_APPT ala 
-	WHERE 
-    ala.CREATE_DTM >= to_date(:i_start_date,'YYYY-MM') 
-    AND ala.CREATE_DTM <= to_date(:i_end_date,'YYYY-MM')
-    AND ala.APPT_TYPE_CD = 'R'
-)
-, SHSC_AA_APPTS AS  (
-	SELECT apptIds.CREATE_DTM, apptIds.APPT_ID FROM DATE_RANGE_APPT_IDS apptIds 
-	INNER JOIN ANA_APPT aa ON aa.APPT_ID = apptIds.APPT_ID 
-		AND aa.SITE_ID = DECODE(:i_siteId,'-',aa.SITE_ID,:i_siteId) 
-		AND aa.APPT_TYPE_CD <> 'N' 
-		AND aa.CREATE_DTM <= to_date(:i_end_date,'YYYY-MM')
-	INNER JOIN ANA_APPT_DETL aad ON aad.APPT_ID = apptIds.APPT_ID 
-		AND aad.ENCNTR_TYPE_ID IN (SELECT * FROM SHSC_AA_ENCNTRTYPE_IDS)
-)
-, APPT_COUNTS AS  (
-	SELECT TO_CHAR(CREATE_DTM,'MM/YY') AS MONTH_YEAR, COUNT(APPT_ID) AS counts
-	FROM SHSC_AA_APPTS 
-	GROUP BY TO_CHAR(CREATE_DTM,'MM/YY')
-)
-SELECT 
-my.MONTH_YEAR, nvl(c.counts,0) AS CHANGE_COUNTS,SITE_CD
-FROM (
-	SELECT SITE_CD 
-	FROM STH_CLINIC 
-	WHERE decode(:i_siteId,'-',NULL,:i_siteId) IS NOT NULL AND SITE_ID =:i_siteId
-	UNION
-	SELECT 'All Clinic' AS SITE_CD FROM DUAL
-	WHERE decode(:i_siteId,'-',NULL,:i_siteId) IS NULL
-) temp
-LEFT JOIN MONTH_YEAR my ON 1=1 
-LEFT JOIN APPT_COUNTS c ON my.MONTH_YEAR = c.MONTH_YEAR
-ORDER BY TO_DATE(my.MONTH_YEAR,'MM/YY');
+;
+END;
+
